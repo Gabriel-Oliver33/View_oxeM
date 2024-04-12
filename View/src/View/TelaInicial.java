4737973fbb314fba6e37ed67611d6d4abd2e2895
Gabriel Silva Oliveira <gabriel.oliver0033@gmail.com>
2024-04-12 15:58:16 -0300

    Update TelaInicial.java

--- View/src/View/TelaInicial.java
+++ View/src/View/TelaInicial.java
@@ -27,6 +27,7 @@ public class TelaInicial extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(600, 400));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
